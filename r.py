e806e23885eb325f1d786ce8b4d75c51f6172579
ashan399 <ashan399@gmail.com>
2019-08-25 00:38:56 -0400

    first

--- r.py
+++ r.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app1 = Flask(__name__)
+app1.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test2.db'
+db = SQLAlchemy(app1)
+
+# il
 
 
 class Post(db.Model):
@@ -14,42 +16,32 @@ class Post(db.Model):
     date_created = db.Column(
         db.DateTime, default=datetime.now().date())
     description = db.Column(db.String(250), unique=False)
+    phone = db.Column(db.String(12), unique=False, default="")
+    email = db.Column(db.String(100), unique=False, default="")
 
-    def __init__(self, name, title, date_created,  description):
+    def __init__(self, name, title, date_created,  description, phone, email):
         self.name = name
         self.title = title
         self.date_created = date_created
         self.description = description
+        self.phone = phone
+        self.email = email
 
     def __repr__(self):
         return '<User %r>' % self.name
 
 
-posts = [
-    {
-        'author': 'ashan p',
-        'title': 'question 1',
-        'content': 'Study for cmsc421',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'ligma',
-        'title': 'question 2',
-        'content': 'second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app1.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         name = request.form['name']
         title = request.form['title']
-
         description = request.form['description']
+        phone = request.form['phone']
+        email = request.form['email']
 
-        p1 = Post(name, title, datetime.now().date(), description)
+        p1 = Post(name, title, datetime.now().date(),
+                  description, phone, email)
 
         try:
             db.session.add(p1)
@@ -62,15 +54,22 @@ def home():
         return render_template('home.html', posts=post_list)
 
 
-@app.route('/about')
+@app1.route('/about')
 def about():
     return render_template('about.html', title='about')
 
 
-@app.route('/user')
-def user():
-    return render_template('about.html', title='about')
+@app1.route('/<user_id>')
+def user(user_id):
+    person = Post.query.filter_by(id=user_id).first()
+    return render_template('user.html', user=person)
+
+
+@app1.route('/posts/<post_id>')
+def post_comments(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    return render_template('comment.html', post=post)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app1.run(debug=True)
